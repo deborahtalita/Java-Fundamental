c425be42e1ec59882c1faa82ec36d4ab89de744b
deborahtalita <marpaung.deborah.tif20@polban.ac.id>
2021-09-22 22:28:20 +0700

    source code soal 3: operators

---
+++ src/TugasPrak2/soal3_operators.java
@@ -0,0 +1,17 @@
+/**
+ * Nama	:	Marpaung, Deborah Talita Kum
+ * NIM	:	201511014						
+ * Kelas:	D3-2A
+*/
+
+package TugasPrak2;
+
+class soal3_operators { //FloatingPoint
+	public static void main(String[] args) {
+		double x = 92.98;
+		System.out.println("x : "+x);
+		System.out.println("Math.round(x) : "+ Math.round(x));
+		int nx = (int) Math.round(x);
+		System.out.println("nx : "+nx);
+	}
+}

4f13b56f7f2745e1ca7fe3e92d52022b45811f2d
deborahtalita <marpaung.deborah.tif20@polban.ac.id>
2021-09-22 21:54:52 +0700

    source code soal 2: variables

---
+++ src/TugasPrak2/soal2_variables2.java
@@ -0,0 +1,18 @@
+/**
+ * Nama	:	Marpaung, Deborah Talita Kum
+ * NIM	:	201511014						
+ * Kelas:	D3-2A
+*/
+
+package TugasPrak2;
+
+public class soal2_variables2 { // Constants2
+	public static final double CM_PER_INCH = 2.54;
+	public static void main(String[] args) {
+		double paperWidth = 8.5;
+		double paperHeight = 11;
+		System.out.println("Paper size in centimeters: " + paperWidth *
+		CM_PER_INCH + " by "+ paperHeight * CM_PER_INCH);
+	}
+
+}

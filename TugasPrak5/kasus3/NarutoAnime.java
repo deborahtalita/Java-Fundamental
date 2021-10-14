e91fdd681bfbb9f88f604c8660d64e2bb3268f00
deborahtalita <marpaung.deborah.tif20@polban.ac.id>
2021-10-15 01:45:35 +0700

    source code TugasPrak5: kasus3

---
+++ TugasPrak5/kasus3/NarutoAnime.java
@@ -0,0 +1,12 @@
+package kasus3;
+
+public class NarutoAnime {
+
+	public static void main(String[] args) {
+		Sasuke s = new Sasuke();
+		s.printDojutsu();
+		Itachi i = new Sasuke();
+		i.printKekkeiGenkai();
+	}
+
+}
